f23a9dc72b9fc2d696c12af87a6b11fad16b21e3
anonymous anonymous <sofiaking2015@u.northwestern.edu>
2021-12-08 16:11:33 +0000

    Direct association between users and rounds

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,6 +1,8 @@
 class Round < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :rounds,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
