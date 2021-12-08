8e43e928ef3c3a5ae0ce94cb54eb435b204e0979
anonymous anonymous <sofiaking2015@u.northwestern.edu>
2021-12-08 16:11:45 +0000

    Whitelist columns in HolesOverview for ActiveAdmin

--- app/admin/holes_overviews.rb
+++ app/admin/holes_overviews.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register HolesOverview do
 
+ permit_params :hole_number, :distance_to_hole, :distance_to_green, :golf_course_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
