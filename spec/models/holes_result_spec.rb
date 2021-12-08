a10491f3a6b454b0aa34ca18eea093b47f97ec2c
anonymous anonymous <sofiaking2015@u.northwestern.edu>
2021-12-08 16:11:32 +0000

    Validate inclusion of holes_results.hole_number

--- spec/models/holes_result_spec.rb
+++ spec/models/holes_result_spec.rb
@@ -12,5 +12,7 @@ RSpec.describe HolesResult, type: :model do
 
     describe "Validations" do
 
+    it { should validate_inclusion_of(:hole_number).in_array([ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18" ]) }
+
     end
 end
